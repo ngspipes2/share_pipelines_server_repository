0c5742e8b36e3070e8a5cb53b1ddc2937f8299d8
BrunoMNDantas <bmndantas@gmail.com>
2018-07-30 19:23:21 +0100

    Update image with upload policy

--- src/main/java/pt/isel/ngspipes/share_pipelines_server_repository/serviceInterface/controller/implementation/ImageController.java
+++ src/main/java/pt/isel/ngspipes/share_pipelines_server_repository/serviceInterface/controller/implementation/ImageController.java
@@ -52,7 +52,10 @@ public class ImageController implements IImageController {
         if(!isValidAccess(PermissionService.Access.Operation.UPDATE, repositoryId, PipelinesRepository.class))
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 
-        imageService.insert(new Image("PipelinesRepository"+repositoryId, IOUtils.toByteArray(file.getInputStream())));
+        if(imageService.getById("PipelinesRepository"+repositoryId) == null)
+            imageService.insert(new Image("PipelinesRepository"+repositoryId, IOUtils.toByteArray(file.getInputStream())));
+        else
+            imageService.update(new Image("PipelinesRepository"+repositoryId, IOUtils.toByteArray(file.getInputStream())));
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
